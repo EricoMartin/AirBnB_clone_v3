d105b78d57a97ccb68d127149f02976250955bfb
EricoMartin <ericomartin.0785@gmail.com>
2024-02-09 15:42:12 -0800

    API route status

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
+""" The Flask web application instance. """
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
@@ -20,29 +20,29 @@ CORS(app, resources={'/*': {'origins': app_host}})
 
 @app.teardown_appcontext
 def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
+    """ The Flask app/request context end event listener. """
     # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
 def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
+    """ Handles the 404 HTTP error code. """
+    return jsonify(error="Not found"), 404
 
 
 @app.errorhandler(400)
 def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
+    """ Handles the 400 HTTP error code. """
+    msg = "Bad request"
+    if isinstance(error, Exception) and hasattr(error, "description"):
         msg = error.description
     return jsonify(error=msg), 400
 
 
-if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+if __name__ == "__main__":
+    app_host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    app_port = int(os.getenv("HBNB_API_PORT", "5000"))
     app.run(
         host=app_host,
         port=app_port,
